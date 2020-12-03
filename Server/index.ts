586bc4ab7b1c7d0eba06cba61282927217c99a72
bgfxc4 <bindermarc@gmx.de>
2020-12-03 19:33:50 +0100

    added routes

--- Server/index.ts
+++ Server/index.ts
@@ -1,13 +1,31 @@
 import express from 'express';
+import { MongoClient } from "mongodb"
 const app = express();
 const PORT = 5558;
 
-app.get('/api/:args', (req,res) => {
-	console.log("got api request args: " + req.params.args)
+var db;
+
+app.get('/api/get/:category/:article', (req,res) => {
+	console.log("got api request article: " + req.params.category + " article: " + req.params.article);
+	res.send("placeholder for article");
 });
 
+app.get('/api/get/:category/', (req,res) => {
+	console.log("got api request category: " + req.params.category);
+	res.send("placeholder for all articles");
+});
+
+app.get('/api/get/', (req,res) => {
+	console.log("got api all categorys");
+	res.send("placeholder for all categorys");
+});
+
+app.use("/", express.static("../Website"))
 
 app.listen(PORT, () => {
 	console.log(`[server]: Server is running at https://localhost:${PORT}`);
-	app.use("/", express.static("../Website"));
+	MongoClient.connect("mongodb://localhost:27017/mpg-website", { useUnifiedTopology: true }, (err, mdb) => {
+		if (err) throw err;
+		db = mdb;
+	})
 });
